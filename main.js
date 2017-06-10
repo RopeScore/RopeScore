40aa76532c42489043291b6070cc66ba9a63d4e7
Svante Bengtson <svante.b37@gmail.com>
2017-06-10 17:12:58 +0200

    win is an array

--- main.js
+++ main.js
@@ -115,7 +115,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
+  if (win === [] || win === null) {
     createWindow()
   }
 })
