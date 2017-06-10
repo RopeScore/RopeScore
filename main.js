c52190b05cea7bfc03443f3165b83e62b352d576
Svante Bengtson <svante.b37@gmail.com>
2017-06-10 17:14:18 +0200

    bah...

--- main.js
+++ main.js
@@ -115,7 +115,8 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === [] || win === null) {
+  console.log(win)
+  if (win.length === 0 || win === null) {
     createWindow()
   }
 })
