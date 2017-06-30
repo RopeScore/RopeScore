c802ae72c0dc36b7d22829b9af216c6aed52cfc4
Svante Bengtson <svante.b37@gmail.com>
2017-06-30 13:12:02 +0200

    hide MissJudges if !MissJudges

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const Config = {
   Debug: false, // default: false
-  LicenceDate: 1497965449147,
+  LicenceDate: 1497966012947,
   version: '2.0.0-au8',
   Eval: true, // default: false
 
@@ -9,7 +9,7 @@ const Config = {
   },
   MissJudges: true, // default: false
   ShowRaw: true, // default: false
-  ShowDC: true, // default: false
+  ShowDC: true, // default: false, (Show Diff and Creat Scores + rank in overall table)
   ShowAllTables: false, // default: false
   CheckStart: 0 // default: 0
 }
