44d01a8658c06dd9b291522d09f271377e331f75
Svante Bengtson <svante@swantzter.se>
2021-08-13 14:13:52 +0000

    Hide "new category" when in a caetgory

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,7 +2,7 @@
   <div>
     <v-btn text v-if="cat.length > 0" :to="`/category/${cat}/results`" class="mr-2" exact>Results</v-btn>
     <v-btn text v-if="cat.length > 0" :to="`/category/${cat}`" class="mr-2" exact>Category</v-btn>
-    <v-btn text to="/new" class="mr-2" exact>New Category</v-btn>
+    <v-btn text v-if="cat.length == 0" to="/new" class="mr-2" exact>New Category</v-btn>
     <v-btn text to="/system" class="mr-2" exact>System</v-btn>
     <v-btn text to="/" class="mr-2" exact>Dashboard</v-btn>
   </div>
